46e9f3e4bd97cd3a4cf73c4792f2e7951adcbc88
Medha Prodduturi <medhaa.prodduturi@gmail.com>
2024-04-25 23:14:48 -0400

    hash

---
+++ hashmaps/dictionary.py
@@ -0,0 +1,6 @@
+dict = {"SE123": "Jack Gross", "SE124": "Amy Jackson", "SE125": "Blaire Williams"}
+
+dict["SE125"] = "Sererna Peters"
+# print(dict.get("SE123"))
+print(dict.keys())
+# print(dict.values())
